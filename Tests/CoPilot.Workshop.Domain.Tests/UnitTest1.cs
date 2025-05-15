6060d8b896d406e3d88516db267344b0874283b4
panospd <anastapd@gmail.com>
2025-05-15 10:51:25 +0300

    Add unit test project for CoPilot.Workshop.Domain
    - Updated solution to include "CoPilot.Workshop.Domain.Tests"
    - Created project file for the unit test project
    - Added necessary NuGet package references for testing
    
    Changes made by Panos Anastasiadis

---
+++ Tests/CoPilot.Workshop.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CoPilot.Workshop.Domain.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
